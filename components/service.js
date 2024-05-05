978945e6210f7d6103116f9fb30ea4e50642023b
“Julian” <jalvarezgaviriajulian@cesde.net>
2024-05-05 18:37:36 -0500

    nt11 feat se actualiza Informes de Análisis & Herramientas Predictivas

--- components/service.js
+++ components/service.js
@@ -1,22 +1,30 @@
 export function Service(){
     document.getElementById("service").innerHTML =  `
 
-    <nav>
+<nav>
+
     <ul class="menu-horizontal">
-     <li>
-         <h1>Informes de Análisis</h1>
-         <p>
-         Sumérgete en nuestros informes de análisis para descubrir una panorámica completa del estado ambiental en el Valle de Aburrá. Nuestros informes ofrecen una descripción detallada de los datos recopilados, incluyendo análisis de tendencias que te ayudarán a comprender cómo está evolucionando el medio ambiente en la región. ¿Quieres saber qué nos depara el futuro? Nuestros informes también contienen predicciones basadas en datos sólidos, permitiéndote visualizar posibles escenarios y tomar decisiones informadas para promover la sostenibilidad ambiental.
-         </p>
-     </li>
-     <li>
-         <h1>Herramientas Predictivas</h1>
-         <p>
-         Nuestras herramientas predictivas son tu guía para anticipar los cambios ambientales en el Valle de Aburrá. ¿Preocupado por la calidad del aire o los niveles de ruido en tu comunidad? Con nuestras herramientas, puedes acceder a modelos de predicción avanzados que te brindan información sobre el índice de calidad del aire y los niveles de ruido futuros. Ya sea que estés planificando proyectos ambientales o simplemente quieras mantenerte informado, nuestras herramientas predictivas te ayudarán a estar un paso adelante y a tomar medidas proactivas para proteger nuestro entorno.
-         Explora nuestros servicios y únete a nosotros en nuestro compromiso con la sostenibilidad ambiental en el Valle de Aburrá. Juntos, podemos hacer una diferencia significativa para las generaciones futuras.
-         
-         </p>
-     </li>
+            <li>
+                <h1>Informes de Análisis</h1>
+                <p>
+                Sumérgete en nuestros informes de análisis para descubrir una panorámica completa del estado ambiental en el Valle de Aburrá. Nuestros informes ofrecen una descripción detallada de los datos recopilados, incluyendo análisis de tendencias que te ayudarán a comprender cómo está evolucionando el medio ambiente en la región. ¿Quieres saber qué nos depara el futuro? Nuestros informes también contienen predicciones basadas en datos sólidos, permitiéndote visualizar posibles escenarios y tomar decisiones informadas para promover la sostenibilidad ambiental.
+                </p>
+            </li>
+
+            <li>
+                <h1>Herramientas Predictivas</h1>
+                <p>
+                Nuestras herramientas predictivas son tu guía para anticipar los cambios ambientales en el Valle de Aburrá. ¿Preocupado por la calidad del aire o los niveles de ruido en tu comunidad? Con nuestras herramientas, puedes acceder a modelos de predicción avanzados que te brindan información sobre el índice de calidad del aire y los niveles de ruido futuros. Ya sea que estés planificando proyectos ambientales o simplemente quieras mantenerte informado, nuestras herramientas predictivas te ayudarán a estar un paso adelante y a tomar medidas proactivas para proteger nuestro entorno.
+                Explora nuestros servicios y únete a nosotros en nuestro compromiso con la sostenibilidad ambiental en el Valle de Aburrá. Juntos, podemos hacer una diferencia significativa para las generaciones futuras.
+                
+                </p>
+            </li>
     </ul>
+
+    <img src="/Assets/RuidoAmbiental.png" alt="ruido trafico vehicular diurno">
+
+</nav>
+
     `
 }
+
